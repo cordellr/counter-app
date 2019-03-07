8a795b2ab62878349598e6129c75dc989484a99e
cordellr <cordell.rainock@gmail.com>
2019-03-07 15:07:57 -0600

    Added getBadgeClass for rendering class dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,10 +14,16 @@ class Counter extends Component {
     //React.Fragment gets rid of unnecessary outside div
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    this.state.count === 0 ? (classes += "warning") : (classes += "primary");
+    return classes;
   }
 
   formatCount() {
