61c1590d4222e9b0a7f04cebe5ecf35a2811f6e9
cordellr <cordell.rainock@gmail.com>
2019-03-07 14:30:19 -0600

    Initial button and value display

---
+++ src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  render() {
+    //React.Fragment gets rid of unnecessary outside div
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
