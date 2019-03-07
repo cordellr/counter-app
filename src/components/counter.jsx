58c490ba8f1884524c4e22e56d58852d982ccd4b
cordellr <cordell.rainock@gmail.com>
2019-03-07 14:39:20 -0600

    Added bootstrap styling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,12 +5,17 @@ class Counter extends Component {
     count: 0
   };
 
+  styles = {
+    fontSize: 18,
+    fontWeight: "bold"
+  };
+
   render() {
     //React.Fragment gets rid of unnecessary outside div
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
