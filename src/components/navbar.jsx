c3e8797f0103f0c126121962efabb4c7d991fb4a
cordellr <cordell.rainock@gmail.com>
2019-03-08 16:27:19 -0600

    Implementation of object destructuring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//Stateless Functional Component
+//since there isn't anything except for a return statement
+//({ totalCounters }) replaces 'props' through object destructuring
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
