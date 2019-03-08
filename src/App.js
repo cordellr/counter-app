d7c0c13399a97d6bf5990f85253c29e294b32ba4
cordellr <cordell.rainock@gmail.com>
2019-03-08 16:10:32 -0600

    Add total counter in navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,60 @@ import "./App.css";
 import Counters from "./components/counters";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  //passes each counter object, sets value to zero, sets state
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  //this handleIncrement method helps maintain single source of truth principle
+  handleIncrement = counter => {
+    //spread operator clones state.counters array
+    const counters = [...this.state.counters];
+    //finds index of counter object passed
+    const index = counters.indexOf(counter);
+    //spread operator clones indexed object
+    counters[index] = { ...counter };
+    //increases value of cloned object
+    counters[index].value++;
+    //sets state of new counters array
+    this.setState({ counters });
+  };
+
+  //handler for delete event called from counter.jsx click event
+  //passes id of counter to be deleted
+  //creates a new arrary, does not directly update state
+  handleDelete = counterID => {
+    //filters for all counters without passed ID
+    const counters = this.state.counters.filter(c => c.id !== counterID);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
         </main>
       </React.Fragment>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,62 +2,21 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  //passes each counter object, sets value to zero, sets state
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  //this handleIncrement method helps maintain single source of truth principle
-  handleIncrement = counter => {
-    //spread operator clones state.counters array
-    const counters = [...this.state.counters];
-    //finds index of counter object passed
-    const index = counters.indexOf(counter);
-    //spread operator clones indexed object
-    counters[index] = { ...counter };
-    //increases value of cloned object
-    counters[index].value++;
-    //sets state of new counters array
-    this.setState({ counters });
-  };
-
-  //handler for delete event called from counter.jsx click event
-  //passes id of counter to be deleted
-  //creates a new arrary, does not directly update state
-  handleDelete = counterID => {
-    //filters for all counters without passed ID
-    const counters = this.state.counters.filter(c => c.id !== counterID);
-    this.setState({ counters });
-  };
-
   render() {
     //values from counters state are passed to counter component
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
